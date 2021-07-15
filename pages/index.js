fbf75993fb56ae18b7d6cb652906e1a43b0a3a26
Caroline Dantas <caroline.dantas@esig.com.br>
2021-07-14 21:58:22 -0300

    Adicionando comunidades

--- pages/index.js
+++ pages/index.js
@@ -24,11 +24,38 @@ function ProfileSidebar(propriedades) {
 
 export default function Home() {
   const usuarioAleatorio = 'anacarolinedantas';
-  const [comunidades, setComunidades] = React.useState([{
-    id: '12802378123789378912789789123896123', 
-    title: 'Eu odeio acordar cedo',
-    image: 'https://alurakut.vercel.app/capa-comunidade-01.jpg'
-  }]);
+  const [comunidades, setComunidades] = React.useState([
+    {
+      id: '12802378123789378912789789123896123', 
+      title: 'Eu odeio acordar cedo',
+      image: 'https://alurakut.vercel.app/capa-comunidade-01.jpg'
+    },
+    {
+      id: '165216421461245142125', 
+      title: 'Deus me disse: desce e arrasa!',
+      image: 'https://img10.orkut.br.com/community/15cf893f0e0d466dd42da2e37d04bde6.jpeg'
+    },
+    {
+      id: '5458613135848731564646', 
+      title: 'Eu tenho medo do PLANTÃO',
+      image: 'https://diariodonordeste.verdesmares.com.br/image/contentid/policy:1.3089845:1621954133/plantao.jpg?f=16x9&h=720&q=0.8&w=1280&$p$f$h$q$w=6826d41'
+    },
+    {
+      id: '5165465655216455424212', 
+      title: 'Eu nunca terminei uma borracha',
+      image: 'https://mercur.vteximg.com.br/arquivos/ids/159006-800-800/borracha_prima_2.jpg?v=636840182910830000'
+    },
+    {
+      id: '5165465655216455424212', 
+      title: 'Tocava campainha e corria',
+      image: 'https://img.freepik.com/fotos-gratis/closeup-dedo-da-mao-de-uma-mulher-pressionando-uma-campainha-ou-campainha-para-casa_49003-1003.jpg?size=626&ext=jpg'
+    },
+    {
+      id: '5165465655216455424212', 
+      title: 'Só mais 5 minutinhos',
+      image: 'https://pediatriadescomplicada.com.br/wp-content/uploads/2016/03/bebe-bocejando-1.jpg'
+    },
+]);
 
   console.log('Nosso teste', );
   const pessoasFavoritas = [
